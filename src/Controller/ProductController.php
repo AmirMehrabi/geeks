c441b2441b85c5de95f90127ef3f489345944390
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2021-07-21 17:34:03 +0430

    Installed Twig and created a model and controller for Procuts.

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+// src/Controller/ProductController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+         /**
+          * @Route("/")
+          */
+          public function index()
+          {
+              $number = 4;
+            return $this->render('index.html.twig', [
+                'number' => $number,
+            ]);
+          }
+
+}
