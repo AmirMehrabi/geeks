40efdcc760d41333b7676ddeb8453ea7d4d9f598
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2021-07-22 18:03:01 +0430

    Working on adding products to the cart

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,46 +2,77 @@
 // src/Controller/ProductController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\AddToCartType;
+use App\Manager\CartManager;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
 
-           /**
-          * @Route("/", name="homepage")
-          */
-          public function index()
-          {
-            return $this->render('index.html.twig');
-          }
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
 
+    /**
+     * @Route("/products", name="products")
+     */
+    public function products()
+    {
 
-         /**
-          * @Route("/products", name="products")
-          */
-          public function products()
-          {
-            
-            $products = $this->getDoctrine()
+        $products = $this->getDoctrine()
             ->getRepository(Product::class)
             ->findAll();
 
-            return $this->render('products.html.twig', [
-              'products' => $products
-            ]);
+        return $this->render('products.html.twig', [
+            'products' => $products,
+        ]);
 
-          }
+    }
 
+    /**
+     * @Route("/product/{id}", name="product.detail")
+     */
+    public function detail(Product $product, Request $request, CartManager $cartManager)
+    {
+        $form = $this->createForm(AddToCartType::class);
 
-         /**
-          * @Route("/cart", name="cart")
-          */
-          public function cart()
-          {
-            return $this->render('cart.html.twig');
-          }
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $item = $form->getData();
+            $item->setProduct($product);
+
+            $cart = $cartManager->getCurrentCart();
+            $cart
+                ->addItem($item)
+                ->setUpdatedAt(new \DateTime());
+
+            $cartManager->save($cart);
+
+            return $this->redirectToRoute('product.detail', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function cart()
+    {
+        return $this->render('cart.html.twig');
+    }
 
 }
